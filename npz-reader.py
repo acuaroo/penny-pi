1a954f41732f1dc7dc8da3c530b087b126749968
Acuaro <67112172+acuaroo@users.noreply.github.com>
2023-02-04 15:33:26 -0700

    fixed npz-reader

--- npz-reader.py
+++ npz-reader.py
@@ -1,17 +1,38 @@
 import numpy as np
 from PIL import Image
+import random
+import os
 
 data_header = 'data'
-data_address = '20230129085106'
-data_index = 0
+data_address = '20230129103343'
+data_index = -1
 
-data = np.load(data_header+'_'+data_address+'.npz')
+#before 20230129103343 uses data_xxxx.npz format
+data = np.load(data_header+'/'+data_address+'/'+'data.npz')
 
+#make a for loop for the length of data['commands']
+for i in range(len(data['commands'])):
+    if i == len(data['commands'])-1:
+        break
 
-img = Image.open(data_header+'/'+data_address+'/'+str(data['ticks'][data_index])+'.png')
-img.show()
+    data_index += 1
 
-print("command: "+str(data['commands'][data_index]))
-print("speed: "+str(data['speed'][data_index]))
-print("tick: "+str(data['ticks'][data_index]))
-print("step interval: "+str(data['step_interval'][data_index]))
+    tick = data['ticks'][data_index]
+    command = data['commands'][data_index+1]
+
+    img = Image.open(data_header+'/'+data_address+'/'+str(tick)+'.png')
+
+    print("command: "+str(command))
+
+    #save PIL image to folder
+    img.save('training-data/'+str(command)+'/'+str(tick)+'_'+str(data_address)+'.png')
+
+    # print("speed: "+str(data['speed'][data_index]))
+    # print("tick: "+str(data['ticks'][data_index]))
+    # print("step interval: "+str(data['step_interval'][data_index]))
+
+#print the length of a directory
+print("process finished")
+print('forward: '+str(len(os.listdir('training-data/F'))))
+print('right: '+str(len(os.listdir('training-data/R'))))
+print('left: '+str(len(os.listdir('training-data/L'))))
