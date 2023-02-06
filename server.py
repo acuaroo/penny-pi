67c33ec98e9cdcd6e69c1014c2326f7511b70479
Acuaro <67112172+acuaroo@users.noreply.github.com>
2023-02-06 13:25:39 -0700

    made self driving toggleable

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ CAMERA_SIZE = 128
 PORT = 1
 DEFAULT_SPEED = 75
 
-interpreter = tflite.Interpreter(model_path='penny-pi/car-23-2-4-ta79.tflite')
+interpreter = tflite.Interpreter(model_path='penny-pi/machine-learning/car-23-2-4-ta79.tflite')
 interpreter.allocate_tensors()
 
 output = interpreter.get_output_details()[0]
@@ -50,7 +50,7 @@ speed_array = []
 step_array = []
 
 on = True
-self_driving = True
+self_driving = False
 recording = False
 session_id = datetime.now().strftime("%Y%m%d%H%M%S")
 
@@ -71,40 +71,102 @@ os.system("mkdir penny-pi/data/"+str(session_id))
 
 print("session id created ("+str(session_id)+")")
 
-
 controller.change_speed(current_speed, p_ena, p_enb)
-
-while self_driving:
-    on = False
-    current_motion = ''
-    rawCapture = PiRGBArray(camera, size=(CAMERA_SIZE, CAMERA_SIZE))
-    camera.capture(rawCapture, format="rgb")
-
-    img_array = rawCapture.array
-    img_array = img_array.reshape(1, CAMERA_SIZE, CAMERA_SIZE, 3)
-    img_array = np.float32(img_array)
-    #img_array = img_array / 255
     
-    interpreter.set_tensor(input['index'], img_array)
-    interpreter.invoke()
+while on:
+    current_motion = ''
 
+    if self_driving:
+        rawCapture = PiRGBArray(camera, size=(CAMERA_SIZE, CAMERA_SIZE))
+        camera.capture(rawCapture, format="rgb")
 
-    predictions = interpreter.get_tensor(output['index'])
-    #np.set_printoptions(precision=2, suppress=True)
-    print(predictions)
+        img_array = rawCapture.array
+        img_array = img_array.reshape(1, CAMERA_SIZE, CAMERA_SIZE, 3)
+        img_array = np.float32(img_array)
+        #img_array = img_array / 255
+        
+        interpreter.set_tensor(input['index'], img_array)
+        interpreter.invoke()
 
-    f = interpreter.get_tensor(output['index'])[0][0]
-    l = interpreter.get_tensor(output['index'])[0][1]
-    r = interpreter.get_tensor(output['index'])[0][2]
+        predictions = interpreter.get_tensor(output['index'])
+        #np.set_printoptions(precision=2, suppress=True)
+        print(predictions)
 
-    if f > r and f > l:
-        current_motion = 'F'
-    elif r > f and r > l:
-        current_motion = 'R'
-    elif l > f and l > r:
-        current_motion = 'L'
+        f = interpreter.get_tensor(output['index'])[0][0]
+        l = interpreter.get_tensor(output['index'])[0][1]
+        r = interpreter.get_tensor(output['index'])[0][2]
 
-
+        if f > r and f > l:
+            current_motion = 'F'
+        elif r > f and r > l:
+            current_motion = 'R'
+        elif l > f and l > r:
+            current_motion = 'L'
+    else:
+        tick += 1
+        data = client_socket.recv(1024)
+        
+        if b'L' in data or b'G' in data:
+            current_motion = 'L'
+        elif b'R' in data or b'I' in data:
+            current_motion = 'R'
+        elif b'F' in data:
+            current_motion = 'F'
+        elif b'B' in data:
+            controller.change_speed(current_speed, p_ena, p_enb)
+            controller.go_backwards()
+            current_motion = ''
+        elif b'S' in data:
+            controller.stop()
+            current_motion = ''
+            
+        if b'W' in data:
+            controller.stop()
+            on = False
+            recording = False
+            break
+        
+        if b'1' in data:
+            current_speed = DEFAULT_SPEED+(6*1)
+            controller.change_speed(DEFAULT_SPEED+(6*1), p_ena, p_enb)
+        elif b'2' in data:
+            current_speed = DEFAULT_SPEED+(6*2)
+            controller.change_speed(DEFAULT_SPEED+(6*2), p_ena, p_enb)
+        elif b'3' in data:
+            current_speed = DEFAULT_SPEED+(6*3)
+            controller.change_speed(DEFAULT_SPEED+(6*3), p_ena, p_enb)
+        elif b'4' in data:
+            current_speed = DEFAULT_SPEED+(6*4)
+            controller.change_speed(DEFAULT_SPEED+(6*4), p_ena, p_enb)
+        elif b'5' in data:
+            current_speed = DEFAULT_SPEED+(6*5)
+            controller.change_speed(DEFAULT_SPEED+(6*5), p_ena, p_enb)
+        elif b'6' in data:
+            current_speed = DEFAULT_SPEED+(6*6)
+            controller.change_speed(DEFAULT_SPEED+(6*6), p_ena, p_enb)
+        elif b'7' in data:
+            current_speed = DEFAULT_SPEED+(6*7)
+            controller.change_speed(DEFAULT_SPEED+(6*7), p_ena, p_enb)
+        elif b'8' in data:
+            current_speed = DEFAULT_SPEED+(6*8)
+            controller.change_speed(DEFAULT_SPEED+(6*8), p_ena, p_enb)
+        elif b'9' in data:
+            current_speed = DEFAULT_SPEED+(6*9)
+            controller.change_speed(DEFAULT_SPEED+(6*9), p_ena, p_enb)
+        elif b'q' in data:
+            current_speed = DEFAULT_SPEED+(6*10)
+            controller.change_speed(DEFAULT_SPEED+(6*10), p_ena, p_enb)
+  
+        if b'U' in data:
+            recording = True
+        elif b'u' in data:
+            recording = False
+        elif b'X' in data:
+            self_driving = True
+        elif b'x' in data:
+            self_driving = False
+        #new "stutter" recording system
+       
     wait_time = 0.5
 
     if current_motion == 'L':
@@ -125,98 +187,13 @@ while self_driving:
     sleep(wait_time)
     controller.stop()
 
-
-while on:
-    tick += 1
-    data = client_socket.recv(1024)
-    current_motion = ''
-    
-    if b'L' in data or b'G' in data:
-        current_motion = 'L'
-    elif b'R' in data or b'I' in data:
-        current_motion = 'R'
-    elif b'F' in data:
-        current_motion = 'F'
-    elif b'B' in data:
-        controller.change_speed(current_speed, p_ena, p_enb)
-        controller.go_backwards()
-        current_motion = ''
-    elif b'S' in data:
-        controller.stop()
-        current_motion = ''
-        
-    if b'W' in data:
-        controller.stop()
-        on = False
-        recording = False
-        break
-    
-    if b'1' in data:
-        current_speed = DEFAULT_SPEED+(6*1)
-        controller.change_speed(DEFAULT_SPEED+(6*1), p_ena, p_enb)
-    elif b'2' in data:
-        current_speed = DEFAULT_SPEED+(6*2)
-        controller.change_speed(DEFAULT_SPEED+(6*2), p_ena, p_enb)
-    elif b'3' in data:
-        current_speed = DEFAULT_SPEED+(6*3)
-        controller.change_speed(DEFAULT_SPEED+(6*3), p_ena, p_enb)
-    elif b'4' in data:
-        current_speed = DEFAULT_SPEED+(6*4)
-        controller.change_speed(DEFAULT_SPEED+(6*4), p_ena, p_enb)
-    elif b'5' in data:
-        current_speed = DEFAULT_SPEED+(6*5)
-        controller.change_speed(DEFAULT_SPEED+(6*5), p_ena, p_enb)
-    elif b'6' in data:
-        current_speed = DEFAULT_SPEED+(6*6)
-        controller.change_speed(DEFAULT_SPEED+(6*6), p_ena, p_enb)
-    elif b'7' in data:
-        current_speed = DEFAULT_SPEED+(6*7)
-        controller.change_speed(DEFAULT_SPEED+(6*7), p_ena, p_enb)
-    elif b'8' in data:
-        current_speed = DEFAULT_SPEED+(6*8)
-        controller.change_speed(DEFAULT_SPEED+(6*8), p_ena, p_enb)
-    elif b'9' in data:
-        current_speed = DEFAULT_SPEED+(6*9)
-        controller.change_speed(DEFAULT_SPEED+(6*9), p_ena, p_enb)
-    elif b'q' in data:
-        current_speed = DEFAULT_SPEED+(6*10)
-        controller.change_speed(DEFAULT_SPEED+(6*10), p_ena, p_enb)
-
-    
-    if b'U' in data:
-        recording = True
-    elif b'u' in data:
-        recording = False
-
-#    if recording and tick % 10 == 0:
-#        #prevent any delay from screwing up the driving
-#        
-#        #Process(target=camera_step, args=(current_motion,)).start()
-#    
-    if current_motion == 'L':
-        #camera_step(current_motion)
-        controller.change_speed(current_speed-(6*2), p_ena, p_enb)
-        controller.turn_left()
-        wait_time = 0.1
-    if current_motion == 'R':
-        #camera_step(current_motion)
-        controller.change_speed(current_speed-(6*2), p_ena, p_enb)
-        controller.turn_right()
-        wait_time = 0.1
-    if current_motion == 'F':
-        controller.change_speed(current_speed, p_ena, p_enb)
-        controller.go_forwards()
-        wait_time = 0.5
-
-    sleep(wait_time)
-    controller.stop()
-    #new "stutter" recording system
-    if recording:
-        camera_step(current_motion)
+    if recording and not self_driving:
+            camera_step(current_motion)
 
 client_socket.close()
 server_socket.close()
 
-np.savez('penny-pi/data/'+str(session_id)+'/data.npz', commands=command_array, ticks=tick_array, speed=speed_array, step_interval=step_array)
+if recording:
+    np.savez('penny-pi/data/'+str(session_id)+'/data.npz', commands=command_array, ticks=tick_array, speed=speed_array, step_interval=step_array)
 
 print("python server closing")
